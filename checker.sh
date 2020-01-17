b49c7ddea9a6bf34efc8e0b3fdb488b25933bc8e
RUBEN CANCHO GASULLA <rubgas@localhost.localdomain>
2020-01-17 10:31:24 +0100

    start again

--- checker.sh
+++ checker.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 SCRIPTNAME="$0"
 ARGS="$@"
-BRANCH="master"
+BRANCH="Your_git_branch"
 
 self_update() {
     cd $SCRIPTPATH
     git fetch
 
-    [ -n $(git diff --name-only origin/$BRANCH) | grep $SCRIPTNAME)] && {
+    [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
         echo "Found a new version of me, updating myself..."
         git pull --force
         git checkout $BRANCH
